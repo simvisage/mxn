33cb4f990b4c0480ec6e34ebcb84145304e48a28
Vancikv <d.kapsak@seznam.cz>
2014-02-15 17:16:56 +0100

    Added tests 03-06

---
+++ mxn/tests/test03_cross_section.py
@@ -0,0 +1,39 @@
+'''
+Created on 15. 2. 2014
+
+@author: Vancikv
+'''
+
+from mxn import \
+    CrossSection, SteelBar, MatrixCrossSection, GeoRect
+
+from matplotlib.figure import \
+    Figure
+
+from matplotlib.backends.backend_agg import \
+    FigureCanvasAgg
+
+import numpy as np
+
+def test03_cross_section_mn():
+    '''Test the moment and normal force calculated for a cross section. 
+    Rectangular cross section with steel reinforcement.
+    '''
+    
+    bar = SteelBar(position=[0.1, 0.45], area=0.0002)
+    ge = GeoRect(height=0.5, width=0.3)
+    cs = CrossSection(reinf=[bar],
+                         matrix_cs=MatrixCrossSection(geo=ge,
+                                                         n_cj=20),
+                         eps_lo=0.002,
+                         eps_up= -0.0033,
+                         )
+    
+    assert np.allclose([cs.M, cs.N], [605.63085424909093, -4763.6924315440474])
+    bar.position = [0.15, 0.35]
+    assert np.allclose([cs.M, cs.N], [595.51085561046102, -4806.0924290168105])
+    cs.eps_lo = 0.010
+    assert np.allclose([cs.M, cs.N], [393.29047407596528, -1821.7451022853184])
+
+if __name__ == '__main__':
+    test03_cross_section_mn()
